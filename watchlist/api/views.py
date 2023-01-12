16297206a792b6d7c30db8a2d5b3d9f72df717b5
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-01-12 23:19:20 +0430

    the get , post, put, delete method worked

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -4,18 +4,39 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serialiser = MovieSerializer(movies, many=True)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serialiser = MovieSerializer(movies, many=True)
+        return Response(serialiser.data)
 
-    return Response(serialiser.data)
+    if request.method == "POST":
+        serialiser = MovieSerializer(data=request.data)
+        if serialiser.is_valid():
+            serialiser.save()
+            return Response(serialiser.data)
+        else:
+            return Response(serialiser.errors)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    movies = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movies)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movies)
+        return Response(serializer.data)
 
-
+    if request.method == 'PUT':
+        movies = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movies, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
     
+    if request.method == 'DELETE':
+        movies = Movie.objects.get(pk=pk)
+        movies.delete()
+        return Response()
+
